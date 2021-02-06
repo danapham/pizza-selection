d89bc9af8036071600e50333895a37ce864f75b3
Dana Pham <danapham12@gmail.com>
2021-02-06 13:33:11 -0600

    saving

---
+++ FavoriteToppings/Program.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace FavoriteToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read a file from disk
+            var json = File.ReadAllText("./toppings.json");
+            //deserialize from json to C#
+            var toppingSelections = JsonConvert.DeserializeObject<List<PizzaSelection>>(json);
+        }
+    }
+}
